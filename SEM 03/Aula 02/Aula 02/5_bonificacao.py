67ebbbce3fd318a2e4e73eeda584258611e7f00f
wagnerrds0 <90592967+wagnerrds0@users.noreply.github.com>
2021-09-19 12:48:39 -0300

    Delete SEM 03/Aula 02 directory

--- SEM 03/Aula 02/Aula 02/5_bonificacao.py
+++
@@ -1,9 +0,0 @@
-#Entrada
-tempo_servico = float(input("Digite o tempo de serviço: "))
-bonus_anual = float(input("Digite o valor do bônus em R$: "))
-
-#Processamento
-bonificacao = tempo_servico * bonus_anual
-
-#Saída
-print("O valor da bonificação anual é de R$: " "%.2f" % bonificacao)
